4644c1c46904d6159ade181b6984093b7eff33cd
Pablo Anglat <panglat@hotmail.com>
2015-10-03 00:42:02 -0300

    First Commit

--- app/app.js
+++ app/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('finalAngularCmeApp', []);
+var app = angular.module('necroProdeApp', ["ngRoute"])
+        .constant("VIEWS", {
+          characterlistview: "view/characterlistview.html",
+          homeview: "view/homeview.html"
+        })
+        .config(["$routeProvider", "VIEWS", function($routeProvider, VIEWS) {
+          $routeProvider.when("/", {
+            controller: "HomeController",
+            templateUrl: VIEWS.homeview
+          }).when("/characters", {
+            controller: "CharacterListController",
+            templateUrl: VIEWS.characterlistview
+          }).otherwise({
+            redirectTo: "/"
+          });
+        }]);
