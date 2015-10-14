945a60e79ad3458f4060de7ae9bd798ad20f8024
Pablo Anglat <panglat@hotmail.com>
2015-10-14 18:43:55 -0300

    add more files, delete unnecessary files

---
+++ data/data.js
@@ -0,0 +1,24 @@
+{
+  "records" : [
+    {
+      "Name": "Jon Snow",
+      "Season": 4,
+      "Chapter": 2
+    },
+    {
+      "Name": "Ned Stark",
+      "Season": 3,
+      "Chapter": 4
+    },
+    {
+      "Name": "Catelyn Stark",
+      "Season": 7,
+      "Chapter": 6
+    },
+    {
+      "Name": "Tyrion Lannister",
+      "Season": 2,
+      "Chapter": 6
+    }
+  ]
+}
